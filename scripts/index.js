c86c1ca307340033f962cac8a6fe2f1211d36914
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-01-07 11:37:53 +0300

    Второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,24 @@
-/*Открытие попапа*/
-var popupOpen = document.querySelector('.profile__edit-button');
-var popupClose = document.querySelector('.popup__button-close');
-var popup = document.querySelector('.popup');
+
+let popupOpen = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__button-close');
+let popup = document.querySelector('.popup');
 
 popupOpen.addEventListener('click', function () {
-  popup.classList.add('popup__open');
+  popup.classList.add('popup_opened');
+  document.querySelector('.popup__input_type_name').defaultValue = document.querySelector('.profile__title').textContent;
+  document.querySelector('.popup__input_type_info').defaultValue = document.querySelector('.profile__subtitle').textContent;
 })
 
 popupClose.addEventListener('click', function () {
-  popup.classList.remove('popup__open');
+  popup.classList.remove('popup_opened');
 })
 
-/*Кнопка сохранить*/
-document.querySelector('.popup__input_type_name').defaultValue = document.querySelector('.profile__title').textContent
-document.querySelector('.popup__input_type_info').defaultValue = document.querySelector('.profile__subtitle').textContent
 
-var sumbit = document.querySelector('.popup__button-sumbit')
-sumbit.addEventListener('click', function () {
-  document.querySelector('.profile__title').textContent = document.querySelector('.popup__input_type_name').value
-  document.querySelector('.profile__subtitle').textContent = document.querySelector('.popup__input_type_info').value
-  popup.classList.remove('popup__open');
+let sumbitForm = document.querySelector('.popup__form');
+
+sumbitForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  document.querySelector('.profile__title').textContent = document.querySelector('.popup__input_type_name').value;
+  document.querySelector('.profile__subtitle').textContent = document.querySelector('.popup__input_type_info').value;
+  popup.classList.remove('popup_opened');
 })
