b1c9614bb7da42c6308ebc6be6a3bf790b058b16
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-01-08 12:14:26 +0300

    Третье ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,13 @@ let popup = document.querySelector('.popup');
 
 popupOpen.addEventListener('click', function () {
   popup.classList.add('popup_opened');
-  document.querySelector('.popup__input_type_name').defaultValue = document.querySelector('.profile__title').textContent;
-  document.querySelector('.popup__input_type_info').defaultValue = document.querySelector('.profile__subtitle').textContent;
+  document.querySelector('.popup__input_type_name').value = document.querySelector('.profile__title').textContent;
+  document.querySelector('.popup__input_type_info').value = document.querySelector('.profile__subtitle').textContent;
 })
 
 popupClose.addEventListener('click', function () {
   popup.classList.remove('popup_opened');
 })
-
 
 let sumbitForm = document.querySelector('.popup__form');
 
